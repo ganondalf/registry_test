a9605ea7a3d945dadf00b48b0fcff125fbcb6de7
ganondalf <cganon@gmail.com>
2014-04-28 10:05:08 -0400

    travis.yml

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,10 @@
 class Person < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-
   def formal_name(id)
     p = Person.find(id)
-    p.first_name = p.first_name.upcase
-    p.last_name = p.last_name.upcase
+    p.first_name = p.first_name.capitalize
+    p.last_name = p.last_name.capitalize
     if p.is_married == false && p.gender == 'female'
       return "Ms. #{p.first_name} #{p.last_name}"
     elsif p.gender == 'female' && p.is_married == true
@@ -13,8 +12,6 @@ class Person < ActiveRecord::Base
     else
       return "Mr. #{p.first_name} #{p.last_name}"
     end
-
   end
-
 end
 
