d2ec9ad1b54758ab561e9b148b649c6c15f6349d
ganondalf <cganon@gmail.com>
2014-04-28 10:24:39 -0400

    edited test file

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -7,16 +7,19 @@ describe "the people index" do
   end
 
   it 'should add "Mrs." to a married female' do
+    Person.create(first_name: "Carolyn", last_name: "Ganon", age: 24, gender: "female", is_married: false)
     visit '/'
     expect(page).to have_content 'Ms. Carolyn Ganon'
   end
 
-  it 'should add "Mr." to all men' do
+  it 'should add "Mrs." to all married women' do
+    Person.create(first_name: "Michelle", last_name: "Ganon", age: 55, gender: "female", is_married: true)
     visit '/'
     expect(page).to have_content 'Mrs. Michelle Ganon'
   end
 
   it 'should add "Mr." to all men' do
+    Person.create(first_name: "Bill", last_name: "Ganon", age: 55, gender: "male", is_married: true)
     visit '/'
     expect(page).to have_content 'Mr. Bill Ganon'
   end
